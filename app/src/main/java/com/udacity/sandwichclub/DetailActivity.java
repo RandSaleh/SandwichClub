453c478cc0960a51b99bc9c48127b66cafe5fe17
RandSaleh <rand_nn@hotmail.com>
2019-05-14 11:15:04 -0400

    fix constant string name

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,6 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,8 +11,6 @@ import android.widget.Toast;
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
-
-import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -50,22 +47,12 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-
-
         populateUI(sandwich);
-
-
-
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
 
-
-
         setTitle(sandwich.getMainName());
-
-        // Enable Up Button to allow the user to navigate back to Parent Activity (MainActivity)
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     private void closeOnError() {
@@ -74,74 +61,98 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI(Sandwich sandwich) {
-        // Initialize and find the Views
 
-        TextView originTextView = findViewById(R.id.origin_tv);
-        TextView alsoKnownAsLabel = findViewById(R.id.also_known_as_label);
+        // Initialize and find the TextView .............
 
-        TextView alsoKnownAsTextView = findViewById(R.id.also_known_tv);
-        TextView descriptionTextView = findViewById(R.id.description_tv);
-        TextView ingredientsTextView = findViewById(R.id.ingredients_tv);
+        TextView txtorigin = findViewById(R.id.origin_tv);
+
+        TextView txtalsoKnownAs = findViewById(R.id.also_known_tv);
+        TextView txtdescription = findViewById(R.id.description_tv);
+        TextView txtingredients = findViewById(R.id.ingredients_tv);
+        ////////---------------------- Lables ---------------------
+        TextView lblDesc = findViewById(R.id.description_label);
+        TextView lblalsoKnown = findViewById(R.id.also_known_as_label);
+        TextView lblOri = findViewById(R.id.origin_label);
+        TextView lblIng = findViewById(R.id.ingredients_label);
+
+        //////////---------------------------------------------
+
+
+
 
         // Get the PlaceOfOrigin
         String originText = sandwich.getPlaceOfOrigin();
-        // if Origin is Empty, set this text
         if (originText.isEmpty()){
-            originTextView.setVisibility(View.GONE);
+            txtorigin.setVisibility(View.GONE);
+            lblOri.setVisibility(View.GONE);
 
-        } else { // otherwise set the actual value
-            originTextView.setText(originText);
+        } else {
+            txtorigin.setText(originText);
         }
 
-        // Get the other names for the Sandwich
+
+///////////-----------------
         List<String> alsoKnownAsList = sandwich.getAlsoKnownAs();
-        // if the List is empty, remove Also Known As Label & TextView
+
         if (alsoKnownAsList.size() == 0){
-            alsoKnownAsLabel.setVisibility(View.GONE);
-            alsoKnownAsTextView.setVisibility(View.GONE);
-        } else { // otherwise display Also Known As Label & TextView
-            alsoKnownAsLabel.setVisibility(View.VISIBLE);
-            alsoKnownAsTextView.setVisibility(View.VISIBLE);
+            lblalsoKnown.setVisibility(View.GONE);
+            txtalsoKnownAs.setVisibility(View.GONE);
+        } else {
+            lblalsoKnown.setVisibility(View.VISIBLE);
+            txtalsoKnownAs.setVisibility(View.VISIBLE);
 
-            String Names=null ;
 
-            // For each name in the List, append
-            // comma and space ", " between them
+            StringBuilder otherNames = new StringBuilder();
+
             for (String otherName : alsoKnownAsList) {
-                Names.concat(otherName);
-                Names.concat(" ");}
-
-
-            alsoKnownAsTextView.setText(Names);
-        }
-
-        // Get the ingredients for the Sandwich
-        List<String> ingredientsList = sandwich.getIngredients();
-        // if the List is empty, set the Text as "Unknown"
-        if (ingredientsList.size() == 0){
-            ingredientsTextView.setVisibility(View.GONE);
-
-        } else { // otherwise set the ingredients using a StringBuilder
-            String ingredients =null;
-
-            // For each ingredient in the List, append
-            // comma and space ", " between them
-            for (String ingredient : ingredientsList) {
-                ingredients.concat(ingredient);
-                ingredients.concat(" ");
+                otherNames.append(otherName).append(", ");
             }
 
+            otherNames.setLength(otherNames.length() - 2);
 
-            ingredientsTextView.setText(ingredients);
+
+            txtalsoKnownAs.setText(otherNames);
         }
 
-        // Get the Description for the Sandwich
-        String description = sandwich.getDescription();
-        // if the Description is empty, set the Text as "Not Available"
-        if (description.isEmpty()) {
-            descriptionTextView.setVisibility(View.GONE);
+
+
+        List<String> ingredientsList = sandwich.getIngredients();
+
+        if (ingredientsList.size() == 0){
+            lblIng.setVisibility(View.GONE);
+            txtingredients.setVisibility(View.GONE);
+
         } else {
-            descriptionTextView.setText(description);
+            StringBuilder ingredients = new StringBuilder();
+
+
+            for (String ingredient : ingredientsList) {
+                ingredients.append(ingredient).append(", ");
+            }
+
+            ingredients.setLength(ingredients.length() - 2);
+
+
+            txtingredients.setText(ingredients);
         }
+
+
+
+
+
+
+        String description = sandwich.getDescription();
+
+        if (description.isEmpty()) {
+            txtdescription.setVisibility(View.GONE);
+        } else {
+            txtdescription.setText(description);
+        }
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -11,95 +11,71 @@ import org.json.JSONObject;
 import java.util.*;
 
 public class JsonUtils {
-    private final   List<String> alsoKnownAs = null ;
-    private final  List<String> ingredients = null ;
+    private final List<String> alsoKnownAs = null;
+    private final List<String> ingredients = null;
+    public static String NAME = "name";
+    public static String MAIN_NAME = "mainName";
+    public static String ALSO_KNOWN_AS = "alsoKnownAs";
+    public static String PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static String DESCRIPTION = "description";
+    public static String IMAGE = "image";
+    public static String INGREDIANT = "ingredients";
+    private static Sandwich sand;
 
-    private static  Sandwich sand ;
 
-
-
-
-    public static Sandwich parseSandwichJson(String json)  {
+    public static Sandwich parseSandwichJson(String json) {
         try {
-///-------------------------------------------------------
-       /* we recieve string contain json data in this form
-        key :jsonObject -- key : string
-                        -- key :ArrayJson
-        key :string
-        key : string
-        key :string
-        key : jsonArray
-
-         */
-
-///////// -----------------Start parsing -------------------------------
 
             JSONObject Starter = new JSONObject(json); /// initializing
 
-            JSONObject name = Starter.getJSONObject("name");
+            JSONObject name = Starter.getJSONObject(NAME);
             //--- parsing name
 
-            String mainName = name.getString("mainName");
-            JSONArray alsoKnownAs_JSONArray = name.getJSONArray("alsoKnownAs");
+            String mainName = name.getString(MAIN_NAME);
+            JSONArray alsoKnownAs_JSONArray = name.getJSONArray(ALSO_KNOWN_AS);
 
             //--- parsing placeOfOrigin
 
-            String placeOfOrigin = Starter.getString("placeOfOrigin");
+            String placeOfOrigin = Starter.getString(PLACE_OF_ORIGIN);
 
             //--- parsing description
 
-            String description = Starter.getString("description");
-
-            //-- parsing image
-
-            String image = Starter.getString("image"); //this one
-
-            //-- parsing ingredients
-
-            JSONArray ingredients_JSONArray = Starter.getJSONArray("ingredients");
+            String description = Starter.getString(DESCRIPTION);
 
 
-///////////////-----------------------------------------------------------------------
+            String image = Starter.getString(IMAGE);
+
+
+            JSONArray ingredients_JSONArray = Starter.getJSONArray(INGREDIANT);
 
 
             ///-------------- converting arrayJSON to Arrylist
 
-            List <String> ingredients_List = convertJSONArrayToList(ingredients_JSONArray);
+            List<String> ingredients_List = convertJSONArrayToList(ingredients_JSONArray);
 
 
-            List <String> alsoKnownAs_List = convertJSONArrayToList(alsoKnownAs_JSONArray);
+            List<String> alsoKnownAs_List = convertJSONArrayToList(alsoKnownAs_JSONArray);
 
             ///-------------- converting  array to Arrylist
 
 
-///----------------------- Assigning the value to the Sandwich Object -------------------
-/* sandwich.setMainName(mainName);
- sandwich.setPlaceOfOrigin(placeOfOrigin);
- sandwich.setDescription(description);
- sandwich.setImage(image);
- sandwich.setIngredients(ingredients_List);
- sandwich.setAlsoKnownAs(alsoKnownAs_List);
-*/
-////----------------------------------------------------------------------------------------
             sand = new Sandwich(mainName, alsoKnownAs_List, placeOfOrigin, description, image, ingredients_List);
 
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        catch (Exception e){e.printStackTrace();}
 
 
         return sand;
     }
 
 
+    public static ArrayList<String> convertJSONArrayToList(JSONArray arrj) throws Exception {
+        ArrayList<String> temp = new ArrayList<String>();
 
-
-    public  static  ArrayList<String> convertJSONArrayToList(JSONArray arrj) throws  Exception {
-        ArrayList <String>temp  = new ArrayList<String>();
-
-        for (int i=0;i<arrj.length();i++){
+        for (int i = 0; i < arrj.length(); i++) {
             temp.add(arrj.getString(i));
         }
-
 
 
         return temp;
